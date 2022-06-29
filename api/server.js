3c0cd1e8695940c3a3bd0c7d2c41f1b1fb180689
Cody Denniston <dennistonc4817@gmail.com>
2022-06-28 22:38:41 -0500

    done

---
+++ api/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+server.get('/api/users', (req,res) => {
+    res.status(200).json({
+        message:"Here is the list of users"
+    })
+})
+
+server.post('/api/register', (req,res) => {
+    req.body = user
+    res.status(201).json(user)
+})
+
+server.post('/api/login', (req,res) => {
+    res.status(200).json({
+        message: "Welcome"
+    })
+})
+
+
+module.exports = server;
